767e60435ed97de3607c3296bdf77e98b0ac04d2
Parwani,Mohammad M.S <m.parwani@student.fontys.nl>
2024-03-13 19:45:11 +0100

    first

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
+// import "./globals.css";
+import "./dashboard/ui/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
